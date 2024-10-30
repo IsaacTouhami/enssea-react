f04b9e3e26acb5712bb5534daf849c00befb0476
Ishak Touhami <isaaco7274@gmail.com>
2024-10-30 21:31:09 +0100

    fixed the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 
+import logo from "../img/logo_enssea_oval-light.png";
 function Header({handleBurger, burgerMenu, darkLight, handleDarkLight}) {
 
 
     return (
         <header id="header">
-        <div className="Logo"><img src="/src/img/logo_enssea_oval-light.png" alt=""/><span>ENSSEA Site Officiel</span></div>
+        <div className="Logo"><img src={logo} alt=""/><span>ENSSEA Site Officiel</span></div>
         <div className="utilities">
             <div className="english"><img src="/src/img/en_gb.gif" alt="" /></div>
             <div className="french"><img src="/src/img/fr_fr.gif" alt=""/></div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
+
 function NavBar({burgerMenu }) {
     const [expandedMenus, setExpandedMenus] = useState({});
 
